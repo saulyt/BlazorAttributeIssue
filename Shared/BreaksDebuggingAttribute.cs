aa16d7a8eccb597a806d8674a72110aaff00656a
saulyt <>
2021-02-18 19:09:36 -0500

    Added attribute with an enum parameter to index,razor to demonstrate that it will prevent all breakpoints from being hit

---
+++ Shared/BreaksDebuggingAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumAttributeIssue.Shared
+{
+    public class BreaksDebuggingAttribute : System.Attribute
+    {
+        public BreaksDebuggingAttribute(MyEnum myEnum)
+        {
+ 
+        }
+    }
+
+    public enum  MyEnum
+    {
+        One,
+        Two,
+        Three,
+        Four,
+        Five
+    }
+}
